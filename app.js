e17f81314ff9facf0ad0b2893b9092f4f5e3dc8e
ANDRÉ ROBERTO TAVARES <25132696@CFP127ALUNO.LOCAL>
2025-08-05 10:05:40 -0300

    Finalização

--- app.js
+++ app.js
@@ -55,7 +55,9 @@ var entradaDeDados = readline.createInterface({
  * NÃO  not  !
 ***************************************************************************************************/
 
-const MESSAGE_ERRO = 'ERRO: a nota informada não pode ser vazia ou com valores negativos!'
+const MESSAGE_ERROR_EMPTY = 'ERRO: a nota informada não pode ser vazia ou com valores negativos!'
+const MESSAGE_ERROR_NOT_NUMBER = 'ERRO: Não é possível calcular com a entrada de letras'
+const MESSAGE_ERROR_OUT_OF_RANGE = 'ERRO: Os valores informados precisam ser entre 0 até 10.'
 
 // Entrada de dados do nome do aluno
 entradaDeDados.question('Digite o nome do aluno: ', function(nome){
@@ -96,29 +98,27 @@ entradaDeDados.question('Digite o nome do aluno: ', function(nome){
                                         entradaDeDados.close()
                                     }else{
 
-                                        if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' || nomeAulno == ''){
-                                            console.log('ERRO: Existem campos que não foram preenchidos')
-                                        
+                                        if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' ) {
+                                            console.log(MESSAGE_ERROR_EMPTY)
+
                                         }else if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)) {
-                                            console.log('Não é possível calcular com a entrada de letras')
+                                            console.log(MESSAGE_ERROR_NOT_NUMBER)
                                         
                                         }else if (Number(nota1) < 0 || Number(nota1)> 10 || Number(nota2) < 0 || Number(nota2)> 10 || Number(nota3) < 0 || Number(nota3)> 10 || Number(nota4) < 0 || Number(nota4)> 10){
-                                            console.log('ERRO: Os valores informados precisam ser entre 0 até 10.')
-                                        }
-                                        //Calculando a media do aluno
-                                        let statusAluno = ''
-                                        // Chama a função que calcula a média, retorna o valor na variável média
-                                        let media = calculoMedia.mediaEscolar(nota1, nota2, nota3, nota4)  
-                                        if (media >= 0 && media < 5) {
-                                            statusAluno = 'REPROVADO'
-                                        }else if (media >= 5 && media < 7) {
-                                            statusAluno = 'em RECUPERAÇÃO'
-                                        }else if (media >= 7 && media <= 10){
-                                            statusAluno = 'APROVADO'
-                                        }
+                                            console.log(MESSAGE_ERROR_OUT_OF_RANGE)
+                                        }else{
+                                            //Chama a função para  gerar a media
+                                            let media = calculoMedia.mediaEscolar(nota1, nota2, nota3, nota4)
+                                            //Chama a função para validar o status do aluno
+                                            let statusAluno = calculoMedia.validarStatus(media)
+
+                                            if(statusAluno){
+                                    
                                         //toFixed() -> permite fixar a qtde de casas decimais 
                                         console.log(`O aluno(a) ${nomeAluno} teve a média: ${media} e ficou ${statusAluno}`)
+                                            }
                                         entradaDeDados.close()
+                                        }
                                     }
                                 })//fecha a nota4
                             }
--- modulo/media.js
+++ modulo/media.js
@@ -16,11 +16,37 @@ function mediaEscolar (valor1, valor2, valor3, valor4){
 
     let media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4))/4
 
-    return media.toFixed(1)
+    return Number (media).toFixed(1)
 }
+    function validarStatus(valor){
+        let media = Number(valor)
+
+        let statusAluno
+
+        if (media >= 0 && media < 5) {
+            statusAluno = 'REPROVADO'
+        }else if(media >= 5 && media < 7) {
+            statusAluno = 'em RECUPERAÇÃO'
+        }else if(media >= 7 && media <= 10){
+            statusAluno = 'APROVADO'
+        }
+        if(statusAluno == undefined){
+            return false
+        }else{
+            return statusAluno
+        }
+    }
+
+
+
+//console.log (mediaEscolar('8.9','4.2', '10', '3.4'))
+//console.log(media)
+//console.log(validarStatus(media))
+
 
 // Permite deixar as funções, variáveis, constantes, e objetos públicos para serem utilizadas em outros
 // arquivos ou projetos
 module.exports = {
-    mediaEscolar
+    mediaEscolar,
+    validarStatus
 }
